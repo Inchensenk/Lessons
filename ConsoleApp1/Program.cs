a0c3a4ec634ac84519e59f4eb6dc72727e71134d
sofoyan gevork <aab8117@gmail.com>
2022-03-31 20:00:24 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    internal class Program
+    {
+        private const string Value = "Привет мир =)";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+
+            int a;
+            a = 5;
+            Console.WriteLine(a);
+
+            string data;
+            data=Console.ReadLine();
+            Console.WriteLine("Привет "+data +"!!!");
+
+        }
+    }
+}
